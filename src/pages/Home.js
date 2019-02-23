fe39496157fd437f482999b960fa7eaf2df6a087
devs7 <devs7g@gmail.com>
2019-02-23 18:59:57 +0200

    Create MainPost in Store

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,43 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import { getPosts } from '../store/actions/gerPostsAction'
-import '../styles/App.css'
 import Posts from '../components/Posts'
+import Post from '../components/Post'
 import { Footer } from '../components/organisms/Footer'
+import { getPosts } from '../store/actions/getPostsAction'
+import { getMainPost } from '../store/actions/getMainPostAction'
+import '../styles/App.css'
 
 class Home extends PureComponent {
   componentDidMount() {
+    this.props.getMainPost()
     this.props.getPosts()
   }
 
   render() {
-    const { posts } = this.props
+    const { posts, mainPost } = this.props
+    // console.log(posts)
+    // console.log(mainPost)
+
     return (
       <>
+        {
+          mainPost.post && <div className='MainPost'>
+            <Post title={mainPost.post.title} img={mainPost.post.img} content={mainPost.post.post} />
+          </div>
+        }
         <div className="Posts">
-        <Posts posts={posts.posts} />
+          <Posts posts={posts.posts} />
         </div>
-        <Footer/>
+        <Footer />
       </>
     )
   }
 }
-const mapSate = ({ posts }) => ({
-  posts
+const mapSate = ({ posts, mainPost }) => ({
+  posts,
+  mainPost
 })
 export default connect(
   mapSate,
-  { getPosts }
+  { getPosts, getMainPost }
 )(Home)
---
+++ src/store/actions/getMainPostAction.js
@@ -0,0 +1,19 @@
+import API from '../../api'
+import { GetMainPost } from '../types'
+
+const getPostStart = () => ({ type: GetMainPost.start })
+const getPostSuccess = payload => ({ type: GetMainPost.success, payload })
+const getPostFailure = payload => ({ type: GetMainPost.failure, payload })
+
+export const getMainPost = () => async dispatch => {
+  try {
+    dispatch(getPostStart())
+
+    const apiCall = await API.getMainPost()
+    const dataApi = await apiCall.json()
+
+    dispatch(getPostSuccess(dataApi.post))
+  } catch (err) {
+    dispatch(getPostFailure('500 internal server error'))
+  }
+}
