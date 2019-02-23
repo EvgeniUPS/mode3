c008f020652adbcc04a8efe0cd432276baa13b82
devs7 <devs7g@gmail.com>
2019-02-23 18:06:21 +0200

    Add other routes

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <Link to='/' children='Home' />
+        <h2>About</h2>
+      </div>
+    )
+  }
+}
+
+export default About
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import App from '../components/App'
+import App from '../pages/Home'
+import Login from '../pages/Login'
+import About from '../pages/About'
 import { Header } from '../components/organisms/Header'
 
 export default () => (
@@ -8,7 +10,8 @@ export default () => (
   <Header/>
   <Switch>
     <Route exact path="/" component={App} />
-    <Route exact path="/about" component={() => <h1>About</h1>} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/login" component={Login} />
   </Switch>
   </>
 )
