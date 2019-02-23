ea07c23b17225f45e26600849434dced9d14dd58
EvgeniUPS <evgeniups@gmail.com>
2019-02-23 19:52:35 +0200

    SelectedPost
    Signed-off-by: EvgeniUPS <evgeniups@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import { Header } from './organisms/Header'
+// import { Header } from './organisms/Header'
 import { getPosts } from '../store/actions/gerPostsAction'
 import '../styles/App.css'
 import Posts from './Posts'
 import { Footer } from './organisms/Footer'
+import { SelectedPost } from './organisms/SelectedPost'
 
 class App extends PureComponent {
   componentDidMount() {
@@ -16,15 +17,17 @@ class App extends PureComponent {
     return (
       <>
         <div className="Posts">
-        <Posts posts={posts.posts} />
+          <Posts posts={posts.posts} />
         </div>
-        <Footer/>
+        <SelectedPost post={this.props.posts.posts} />
+
+        <Footer />
       </>
     )
   }
 }
 const mapSate = ({ posts }) => ({
-  posts
+  posts,
 })
 export default connect(
   mapSate,
