1f8fdef8c230cdd7489a4493e5616da5956c4922
devs7 <devs7g@gmail.com>
2019-02-23 22:13:40 +0200

    Created commits

--- src/components/atom/Div.js
+++ src/components/atom/Div.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components'
 
-export const JokeBlock = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  width: ${props => props.theme.sizes.joke};
-  min-width: ${props => props.theme.sizes.joke};
-`
 export default styled.div`
   margin: ${props => props.margin};
   padding: ${props => props.padding};
@@ -16,6 +9,7 @@ export default styled.div`
   min-width: ${props => props.minWidth};
   flex-direction: ${props => props.flexDirection};
   color: ${props => props.theme.colors.darkOrange};
+  align-items: ${props => props.alignItems};
   border-radius: ${props => props.borderRadius || '15px'};
   justify-content: ${props => props.between && 'space-between'};
   display: ${props => props.display || 'flex'};
